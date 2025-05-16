48099575e7b2d3ff6bffb67177ea9932d0b91e75
AdrM0Hdz <adrian.hernandez.p0@gmail.com>
2025-05-16 11:40:28 -0600

    got my formatter working

--- multi-regex/popup.tsx
+++ multi-regex/popup.tsx
@@ -1,45 +1,44 @@
 import { useState } from "react"
 
 function getIdOfCurrentTab() {
-chrome.tabs.query(
-  {currentWindow: true, active : true},
-  function(tabArray){}
-)
+  chrome.tabs.query(
+    { currentWindow: true, active: true },
+    function (tabArray) {}
+  )
 }
 
-
 function IndexPopup() {
-   const [tabId, setTabId] = useState<number>(0);
-   chrome.tabs.query(
-     {currentWindow: true, active : true},
-     function(tabArray){
-            let tab = tabArray[0];
-            setTabId(tab.id);
-     }
-   )
+  const [tabId, setTabId] = useState<number>(0)
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabArray) {
+    let tab = tabArray[0]
+    setTabId(tab.id)
+  })
 
   if (tabId != 0) {
-
     chrome.scripting
-    .executeScript({
-      target : {tabId : tabId},
-      world: "MAIN", func: () => alert("hi"),
-    }).then(() => console.log("script loaded"))
-
+      .executeScript({
+        target: { tabId: tabId },
+        world: "MAIN",
+        func: () => alert("hi")
+      })
+      .then(() => console.log("script loaded"))
   }
 
-  console.log(tabId);
+  console.log(tabId)
   const [data, setData] = useState("")
   return (
-     <div
+    <div
       style={{
         padding: 16
       }}>
-      Hello 
-      <div onClick={() =>{
-                console.log("hi");
-      }}>Hi here </div>
-     </div>
+      Hello
+      <div
+        onClick={() => {
+          console.log("hi")
+        }}>
+        Hi here{" "}
+      </div>
+    </div>
   )
 }
 
